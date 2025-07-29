fee56abc608d034e64b92e1c832513bb7089bf84
asteriskzuo <zuoyuhsywn@hotmail.com>
2025-07-29 20:18:55 +0800

    test

--- test/src/version.ts
+++
@@ -1,4 +0,0 @@
-// This file is generated automatically. Please do not edit it manually. If necessary, you can run the 'scripts/bundle-icons.js' script to generate it again.
-
-const VERSION = '1.8.0-beta.1';
-export default VERSION;

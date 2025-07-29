ca2fd2a7e911f23f3ce5d64be6150bbf55c39246
asteriskzuo <zuoyuhsywn@hotmail.com>
2025-07-29 21:08:00 +0800

    chore: update

--- restore.sh
+++ restore.sh
@@ -1,9 +1,9 @@
-# set -e
+set -e
 set -u
 
 target_dir="/Users/asterisk/Codes/zuoyu/react-native-chat-sdk-cn"
 source_dir="/Users/asterisk/Codes/rn/react-native-chat-sdk-rn72"
 
-cp -p -R -f ${source_dir}/src ${target_dir}/test
-# cp -p -R -f ${source_dir}/docs ${target_dir}
-# cp -p -R -f ${source_dir}/scripts ${target_dir}
+cp -p -R -f ${source_dir}/src ${target_dir}
+cp -p -R -f ${source_dir}/docs ${target_dir}
+cp -p -R -f ${source_dir}/scripts ${target_dir}
